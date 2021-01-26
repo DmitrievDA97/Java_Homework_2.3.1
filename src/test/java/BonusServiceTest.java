fec989020df7ec460f855c4af91401bf39a645e4
Denis Dmitriev <denitos97@gmail.com>
2021-01-26 13:50:24 +0300

    Added 2 tests

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -28,4 +28,28 @@ class BonusServiceTest {
 
         assertEquals(expected, actual);
     }
+    @Test
+    void shouldCalculateNoRegisteredAndOverLimit() {
+        BonusService service = new BonusService();
+
+        long amount = 100_000_60;
+        boolean registered = false;
+        long expected = 500;
+
+        long actual = service.calculate(amount, registered);
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    void shouldCalculateNoRegisteredAndUnderLimit() {
+        BonusService service = new BonusService();
+
+        long amount = 1000_60;
+        boolean registered = false;
+        long expected = 10;
+
+        long actual = service.calculate(amount, registered);
+
+        assertEquals(expected, actual);
+    }
 }
